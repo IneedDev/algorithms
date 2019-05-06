1cb6bb4ddb57d4db9c9311f35912ca12f55598d6
p.romaniuk.pl@gmail.com <p.romaniuk.pl@gmail.com>
2019-05-06 18:30:08 +0200

    Algorithms - bubble sort

--- src/ShufflingRandomNumbers/Main.java
+++ src/ShufflingRandomNumbers/Main.java
@@ -13,16 +13,13 @@ public class Main {
 
         Lottery lottery = new Lottery(49);
         System.out.println(lottery.toString());
-
         System.out.println(Arrays.toString(lottery.pick(6)));
-
 
         Lottery lottery1 = new Lottery(MAX_NUMBER);
         for (int i=0;i<NUMBER_OF_TICKETS;i++){
             int[] selection=lottery1.pick(NUMBER_TO_PICK);
             String selectString = Arrays.toString(selection);
             System.out.printf("Ticket # %d: %s\n",i,selectString);
-
+        }
     }
 }
-    }
